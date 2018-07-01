fb2c07e209ae375dc7e1117ae9416db43423a285
Keith Kwong <chubbster7134@gmail.com>
2018-07-01 14:09:12 -0700

    Sign up firebase started

---
+++ javascript/login.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBiPEsbVeOpppIEYFUeZYlBXzNinARr09w",
+    authDomain: "cac2018-648ca.firebaseapp.com",
+    databaseURL: "https://cac2018-648ca.firebaseio.com",
+    projectId: "cac2018-648ca",
+    storageBucket: "cac2018-648ca.appspot.com",
+    messagingSenderId: "275374546726"
+  };
+  firebase.initializeApp(config);
